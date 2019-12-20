8f8668b4ae966ee602028d0c9148373fdd63d417
Pamkaj Botrel <pamkaj.botrel@cgi.com>
2019-12-20 06:45:16 +0100

    trying okta auth, not working

--- angular/src/app/services/real/add-header-interceptor.ts
+++ angular/src/app/services/real/add-header-interceptor.ts
@@ -16,6 +16,7 @@ export class AddHeaderInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Clone the request to add the new header
     const accessToken = this.oktaAuth.getAccessToken();
+    console.log(accessToken);
     const clonedRequest = req.clone({ headers: req.headers.set('Authorization', 'Bearer' + accessToken) });
 
     // Pass the cloned request instead of the original request to the next handle
